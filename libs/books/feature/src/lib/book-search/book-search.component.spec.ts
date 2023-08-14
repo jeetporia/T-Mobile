a23596d66076bf4e90ff378f7a006ba2d9a89dd0
jporia <jporia@lenovo.com>
2023-08-14 17:16:13 +0530

    Task 2: Adding instant search

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -4,24 +4,45 @@ import { SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { StoreModule, Store } from '@ngrx/store';
+import { searchBooks } from '../../../../data-access/src/lib/+state/books.actions';
 
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
+  let store: Store;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [
+        BooksFeatureModule,
+        NoopAnimationsModule,
+        SharedTestingModule,
+        StoreModule.forRoot({}),
+      ],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookSearchComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(Store);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
   });
+
+  it('should dispatch searchBooks action when the search term is provided', () => {
+    spyOn(store, 'dispatch');
+    component.ngOnInit();
+    component.searchForm.patchValue({
+      term: 'sample search term',
+    });
+    component.searchBookInput();
+    expect(store.dispatch).toHaveBeenCalledWith(
+      searchBooks({ term: 'sample search term' })
+    );
+  }); 
 });

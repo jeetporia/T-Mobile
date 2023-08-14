bc87a80cbb9ed389747b42306a1b5b8b4026f771
jporia <jporia@lenovo.com>
2023-08-14 17:57:02 +0530

    Task 3: Undo add and remove from reading list

--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -4,24 +4,45 @@ import { SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
+  let snackBar: MatSnackBar;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule, MatSnackBarModule],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookSearchComponent);
     component = fixture.componentInstance;
+    snackBar = TestBed.inject(MatSnackBar);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
+  });
+
+  it('should show a snackbar when a book is added to the reading list', () => {
+    const book = {
+      id: '1',
+      title: 'Test Book',
+      authors: ['Test Author'],
+      description: 'This is a test book.',
+      publisher: 'Test Publisher',
+      publishedDate: '2023-08-05',
+      coverUrl: 'test-cover-url'
+    };
+
+    spyOn(snackBar, 'open').and.callThrough();
+
+    component.addBookToReadingList(book);
+
+    expect(snackBar.open).toHaveBeenCalledWith('Test Book Added', 'Undo', jasmine.any(Object));
   });
 });
--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
-
+import { Observable, Subscription } from 'rxjs';
+import { getReadingList, removeFromReadingList, addToReadingList } from '@tmo/books/data-access';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Book, ReadingListItem } from '@tmo/shared/models';
 @Component({
   selector: 'tmo-reading-list',
   templateUrl: './reading-list.component.html',
   styleUrls: ['./reading-list.component.scss']
 })
-export class ReadingListComponent {
-  readingList$ = this.store.select(getReadingList);
+export class ReadingListComponent implements OnDestroy {
+  readingList$: Observable<any>;
+  private subscription: Subscription = new Subscription();
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private readonly snackBar: MatSnackBar) {
+    this.readingList$ = this.store.select(getReadingList);
+  }
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+    this.openSnackBar(item.title + ' Removed', 'Undo', item);
+  }
+  
+  openSnackBar(message: string, action: string, book) {
+    const snackBarRef = this.snackBar.open(message, action, {
+      duration: 3000
+    });
+  
+    this.subscription.add(
+      snackBarRef.onAction().subscribe(() => {
+        this.addBookToReadingList(book);
+        snackBarRef.dismiss();
+      })
+    );
+  }
+  
+  addBookToReadingList = (book: Book) => {
+    this.store.dispatch(addToReadingList({ book }));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
